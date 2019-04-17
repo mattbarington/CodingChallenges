e49ed745671e58e79e72700fb8e8c7306151362e
Matthew Ovenden <movenden@ucsc.edu>
2019-04-17 16:23:10 -0700

    box/row/col abstraction

--- SudokuSolver/Sudoku.js
+++ SudokuSolver/Sudoku.js
@@ -5,20 +5,38 @@ class Board {
 		for (var i = 0 ; i < 9; i++)
 			notes[i] = new Set[9];
 		this.unsolved = 81;
+		this.rows = new Set[9];
+		this.cols = new Set[9];
+		this.boxes = new Set[9];
+		for (i = 0; i < 9; i++) {
+			for (j = 0; j < 9; j++) {
+				putVal(i, j, board[i][j]);
+			}
+		}
 	}
 
-	getColumn(col) {
-		return []
+	boxNum(r,c) {
+		return (c / 3) + (r / 3)*3;
 	}
 
-	getRow(row) {
-		return []
+	getCol(row) {
+		return this.rows[row];
+	}
+
+	getRow(col) {
+		return this.cols[col];
 	}
 
 	getBox(r,c) {
-		return [[]]
+		return this.boxes[boxNum];
 	}
-	
+
+	putVal(r, c, val) {
+		this.rows[r].add(val);
+		this.cols[c].add(val);
+		this.boxes[boxNum(r,c)].add(val);
+	}
+
 	solve() {
 		clearRow();
 		clearCol();
