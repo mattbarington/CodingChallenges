de285fe7ac6c49a04f9e8996f6a72974daa8d301
unknown <reubenthegold@gmail.com>
2019-04-17 15:51:32 -0700

    :)

--- SudokuSolver/Sudoku.js
+++ SudokuSolver/Sudoku.js
@@ -1,1 +1,31 @@
+class Board {
+	constructor(board) {
+		this.board = board;
+		this.notes = new Array[9];
+		for (var i = 0 ; i < 9; i++)
+			notes[i] = new Set[9];
+		this.unsolved = 81;
+	}
+	
+	solve() {
+		clearRow();
+		clearCol();
+		clearBox();
+	}
+	
+	clearRow() {
+		
+	}
+	
+	clearCol() {
+		
+	}
+	
+	clearBox() {
+		
+	}
+	
+}
+
+
 console.log("hello");
